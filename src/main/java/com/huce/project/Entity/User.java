66142c5c73a946ef77f69b4eb523b2af04ab7054
tuan2612 <94676218+tuan2612@users.noreply.github.com>
2024-04-25 12:07:07 +0700

    them geter

--- src/main/java/com/huce/project/Entity/User.java
+++ src/main/java/com/huce/project/Entity/User.java
@@ -23,7 +23,7 @@ public class User {
     
     @Column(name = "email", length = 100, nullable=false)
     private String email;
-   
+    
     public User() {}
     
 
@@ -33,5 +33,36 @@ public class User {
         this.email = email;
     }
 
-}
+    public long getUserID() {
+        return userID;
+    }
 
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
