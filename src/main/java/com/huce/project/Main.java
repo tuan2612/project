293f049eb1c38083b0707cb48d1c284dee8f7276
hoangquydz0212@gmail.com <hoangquydz0212@gmail.com>
2024-04-26 09:50:34 +0700

    test1

--- src/main/java/com/huce/project/Main.java
+++ src/main/java/com/huce/project/Main.java
@@ -1,11 +1,28 @@
 package com.huce.project;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Main {
+import com.huce.project.Service.FilesStorageService;
 
+import jakarta.annotation.Resource;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner{
+@Resource
+	FilesStorageService storageService;
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+    @Override
+  	public void run(String... args) {
+    try {
+      storageService.deleteAll();
+      storageService.init();
+    } catch (Exception e) {
+      // Handle initialization errors gracefully
+      System.err.println("Error occurred during initialization: " + e.getMessage());
+    }
+  }
 }
